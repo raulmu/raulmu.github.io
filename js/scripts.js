3a8630112628eb7d224e73956e142b563217d176
raulmu <raulmu@outlook.com>
2020-04-25 22:18:19 -0300

    v0 commit includes social medias

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-console.log('carregou javascript');
+document.getElementById("twitter-icon").addEventListener("click", function( event ) {
+    window.open('https://twitter.com/rauluhlmann')
+  }, false);
+document.getElementById("mail-icon").addEventListener("click", function( event ) {
+    document.location.href = "mailto:rmuhlmann@gmail.com";
+}, false);
+document.getElementById("whatsapp-icon").addEventListener("click", function( event ) {
+  window.open("https://bit.ly/whatsraulmu");
+}, false);
+document.getElementById("github-icon").addEventListener("click", function( event ) {
+  window.open("https://github.com/raulmu");
+}, false);
